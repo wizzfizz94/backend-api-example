47505633f20a28a59a6557ed70e4dca95708ce09
zacn <zacn@thearcsgroup.com>
2023-12-02 11:32:25 +0800

    Added basic auth.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,18 @@
 import {Middleware} from "koa";
 import {Unauthorized} from "http-errors";
 
+const basicAuth = require('basic-auth');
+
+const username: string = "guest";
+const password: string = "password";
+
 export const auth: Middleware =
     async (ctx, next) => {
-        // Put a basic auth solution here
+        const user = basicAuth(ctx);
+
+        if (user && user.name === username && user.pass === password) {
+            return next();
+        }
+        
         throw new Unauthorized();
     };
