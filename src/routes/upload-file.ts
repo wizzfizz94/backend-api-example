88e96011018a7dfb7f1aaba6746d83ba0ad5afd2
zacn <zacn@thearcsgroup.com>
2023-12-04 13:31:57 +0800

    Files upload to aws S3.

--- src/routes/upload-file.ts
+++ src/routes/upload-file.ts
@@ -1,9 +1,32 @@
 import { Context, Middleware } from "koa";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { File } from "formidable"
+
+const fs = require("fs");
+const client = new S3Client({});
 
 export const uploadFile: Middleware =
   async (ctx: Context) => {
-    if (ctx.request.files) {
-      console.log(ctx.request.files.file);
-      // upload file to s3
+    if (ctx.request.files && ctx.request.files.files) {
+      const files: File[] = Array.isArray(ctx.request.files.files) ?
+        ctx.request.files.files :
+        [ctx.request.files.files]
+
+      for (const file of files) {
+        const body = fs.createReadStream(file.filepath);
+
+        const command = new PutObjectCommand({
+          Bucket: "backend-challenge-image-uploads",
+          Key: `${Date.now().toString()}-${file.originalFilename}`,
+          Body: body
+        });
+      
+        try {
+          const res = await client.send(command);
+          console.log(res);
+        } catch (err) {
+          console.error(err);
+        }
+      }
     }
   }
